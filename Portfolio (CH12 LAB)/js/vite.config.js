ebb86fc5e7d1417f21e934b703bd1352cd519a91
Deshaun Brown <deshaunbrown933@yahoo.com>
2025-08-19 11:30:19 -0500

    Commit Summary

--- Portfolio (CH12 LAB)/js/vite.config.js
+++ Portfolio (CH12 LAB)/js/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Portfolio (CH12 LAB)/distribution/my-final-app/',
+  base: 'https://deshaun-brown.github.io/my-final-app',
   plugins: [react()],
 }) 
